295232c4fdc288e28eab45e1153a4e1e8fbfd519
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2026-04-28 04:44:05 +0530

    feat: add custom error types

---
+++ src/types/errors.ts
@@ -0,0 +1,27 @@
+export class AuthenticationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'AuthenticationError';
+  }
+}
+
+export class ValidationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'ValidationError';
+  }
+}
+
+export class NetworkError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'NetworkError';
+  }
+}
+
+export class DocumentError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'DocumentError';
+  }
+}
